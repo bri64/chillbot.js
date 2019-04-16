3f01938e5391c295c5e50921c3eff7661fa65c93
Dan Schmitt <35700921+djs5008@users.noreply.github.com>
2019-04-16 19:06:28 -0400

    Playlist Search (#5)
    * removed exclamation mark from message (being added to url)
    
    * added playlist searching functionality
    
    * fixed song filter logic

--- src/commands/search.js
+++ src/commands/search.js
@@ -5,17 +5,32 @@ const Command = require("./command");
 exports.SearchCommand = class SearchCommand extends Command {
     async execute(params) {
         await super.execute(params);
-        let query = this.args.join(" ");
+
+        if (this.args.length <= 0) {
+            this.msg.reply(`Invalid arguments. !${this.command.toLowerCase()} [ playlist ] { url }`);
+            return;
+        }
+
+        let isPlaylist = this.args[0].toUpperCase() === "PLAYLIST";
+        let query = (isPlaylist)
+            ? this.args.slice(1, this.args.length).join(" ")
+            : this.args.join(" ");
+
         try {
-            let results = await this.musicManager.search(this.guild, query);
             let fields = [];
-            for (let result of results) {
+            let results = [];
+            if (isPlaylist) {
+                results = await this.musicManager.searchPlaylist(this.guild, query);
+            } else {
+                results = await this.musicManager.search(this.guild, query);
+            }
+            results.forEach(result => {
                 fields.push({
                     name: result.title,
                     value: result.url,
                     inline: true
                 });
-            }
+            });
             this.msg.reply(new RichEmbed({
                 title: 'Search Results',
                 fields: fields,
