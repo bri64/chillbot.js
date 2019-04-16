f61b6dff916bef81498ecf25639dc0b530219b25
Brian <brianstrains@gmail.com>
2019-04-16 00:08:21 -0400

    Shuts down cleanly

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const MusicManager = new (require("./src/music-manager"))(Client, Config.tokens)
 const CommandManager = new (require("./src/command-manager"))(MusicManager);
 const EventListener = new (require("./src/event-listener"))(Client, MusicManager, CommandManager);
 
+process.on('SIGINT', async () => {
+   console.info("Shutting down...");
+   await MusicManager.shutdown();
+   console.info("Goodbye!");
+   process.exit(0);
+});
+
 ClientHandler.setup()
     .then((result) => {
         console.info(result);
